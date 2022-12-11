c797467dfa92bc2d65898de456dbb0df8ff7bd0e
DimitarZlatev10 <aguerofc@abv.bg>
2022-12-12 01:36:19 +0200

    added phone number requirement for register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -12,13 +12,20 @@ export class RegisterComponent implements OnInit {
   errorMessage: string = '';
   username: string = '';
   email: string = '';
+  phoneNumber: string = '';
   password: string = '';
   repass: string = '';
+
   constructor(
     private httpClient: HttpClient,
     private router: Router,
     private localService: LocalService
   ) {}
+
+
+  isNumberValid(){
+    return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(this.phoneNumber)
+  }
 
   isEmailValid() {
     return /^[a-zA-Z0-9]+\@[a-zA-Z]{2,}\.[a-zA-Z0-9]{2,}$/.test(this.email);
@@ -27,7 +34,7 @@ export class RegisterComponent implements OnInit {
     return this.password.length < 6;
   }
   isRepassValid() {
-      return this.password == this.repass;
+    return this.password == this.repass;
   }
 
   registerUser() {
@@ -35,15 +42,13 @@ export class RegisterComponent implements OnInit {
       .post(`http://localhost:3000/users/register`, {
         username: this.username,
         email: this.email,
+        phoneNumber: this.phoneNumber,
         password: this.password,
         repass: this.repass,
       })
       .subscribe({
         next: (data) => {
           this.localService.saveData('token', this.email);
-
-          console.log(localStorage);
-          console.log(data);
           this.router.navigate(['/']);
         },
         error: (err) => {
