815cf78b33b8a87b5c60008547754829b4e9c79b
DimitarZlatev10 <aguerofc@abv.bg>
2022-12-11 05:12:25 +0200

    added new api calls

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,11 +13,33 @@ export class ApiService {
     return this.httpClient.get(`${apiUrl}/users`);
   }
 
+  getUserId(email: string) {
+    return this.httpClient.post(`${apiUrl}/users/getId`, { email });
+  }
+
   loadShirts() {
     return this.httpClient.get(`${apiUrl}/shirts`);
   }
 
   loadShirtById(id: any) {
     return this.httpClient.get(`${apiUrl}/shirts/` + id);
+  }
+
+  loadShirtByTeamName(teamName: string | null) {
+    return this.httpClient.get(`${apiUrl}/shirts/teamName/` + teamName);
+  }
+
+  addToWishlist(shirtId: string, userId: string) {
+    return this.httpClient.post(`${apiUrl}/shirts/wishlist`, {
+      shirtId,
+      userId,
+    });
+  }
+
+  removeFromWishlist(shirtId: string, userId: string) {
+    return this.httpClient.post(`${apiUrl}/shirts/removeWishlist`, {
+      shirtId,
+      userId,
+    });
   }
 }
