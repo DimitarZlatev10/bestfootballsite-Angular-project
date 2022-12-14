e355d17c52b832a8b2afbd8c2ed5d103c8638080
DimitarZlatev10 <aguerofc@abv.bg>
2022-12-14 04:28:29 +0200

    added buy button and check if user can access it

--- src/app/products/details/details.component.ts
+++ src/app/products/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { IShirt } from 'src/app/interfaces/shirt';
+import { LocalService } from 'src/app/local.service';
 
 @Component({
   selector: 'app-details',
@@ -11,11 +12,27 @@ import { IShirt } from 'src/app/interfaces/shirt';
 export class DetailsComponent implements OnInit {
   id: string | null = '';
   item: IShirt[] | any = [];
+  quantity: number;
+  isLogged: string | null = null;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+  buyButton() {
+    if (this.quantity == 0) {
+      return;
+    }
+    console.log(this.quantity);
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private apiService: ApiService,
+    private localService: LocalService
+  ) {}
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log(this.id);
+
+    this.isLogged = this.localService.getData('token');
+    console.log(this.isLogged);
 
     this.apiService.loadShirtById(this.id).subscribe({
       next: (value) => {
