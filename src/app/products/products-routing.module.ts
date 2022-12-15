087be7400b3c821171dc70b2544d58b31b7e46f3
DimitarZlatev10 <aguerofc@abv.bg>
2022-12-15 05:03:11 +0200

    finished products routes

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -52,24 +52,92 @@ const routes: Routes = [
               team: 'Barcelona',
             },
           },
+          {
+            path: 'Real Madrid',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Real Madrid',
+            },
+          },
+          {
+            path: 'Atletico Madrid',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Atletico Madrid',
+            },
+          },
+          {
+            path: 'Bayern Munich',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Bayern Munich',
+            },
+          },
+          {
+            path: 'Borussia Dortmund',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Borussia Dortmund',
+            },
+          },
+          {
+            path: 'RB Leipzig',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'RB Leipzig',
+            },
+          },
+          {
+            path: 'Juventus',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Juventus',
+            },
+          },
+          {
+            path: 'Milan',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Milan',
+            },
+          },
+          {
+            path: 'Inter',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Inter',
+            },
+          },
+          {
+            path: 'Napoli',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Napoli',
+            },
+          },
+          {
+            path: 'PSG',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'PSG',
+            },
+          },
+          {
+            path: 'Marseille',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Marseille',
+            },
+          },
+          {
+            path: 'Olympic Lyon',
+            component: ProductsTemplateComponent,
+            data: {
+              team: 'Olympic Lyon',
+            },
+          },
         ],
       },
-      // {
-      //   path: 'spanish-league',
-      //   children: [],
-      // },
-      // {
-      //   path: 'german-league',
-      //   children: [],
-      // },
-      // {
-      //   path: 'italian-league',
-      //   children: [],
-      // },
-      // {
-      //   path: 'french-league',
-      //   children: [],
-      // },
       {
         path: 'shirts/details/:id',
         component: DetailsComponent,
